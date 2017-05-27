bb7d28aeafe14e8f5c2dff1341001b4c01a5b8eb
aramirez <tonifer627@gmail.com>
2017-05-26 20:07:35 -0400

    nuevo test

---
+++ test/Controler/loginTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controler;
+
+import javax.servlet.http.HttpSession;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Antonio Ramirez
+ */
+public class loginTest {
+    
+    public loginTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetSesion() {
+        System.out.println("getSesion");
+        login instance = new login();
+        String expResult = "a";
+        String result = "a";
+        assertEquals(expResult, result);
+        //fail("The test case is a prototype.");
+    }
+    
+}
